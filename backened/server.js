95470c6e57dcedcaea503f901450b2ebcad9c9e1
Shweta <shwetakoffficial@gmail.com>
2024-02-28 15:32:04 +0530

    update connecttodb in  server.js

--- backened/server.js
+++ backened/server.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-import connectToMongoDB from "./db/connectToMongoDB.js";
+import connectToMongoDB from "./db/connecttoMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
@@ -13,7 +13,7 @@ const __dirname = path.resolve();
 
 dotenv.config();
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
