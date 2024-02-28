5af48e572b38a4a69fddbee6ddd162f721a4f892
Shweta <shwetakoffficial@gmail.com>
2024-02-28 12:40:17 +0530

    debug App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import SignUp from './pages/signup/SignUp.jsx';
-import { Toaster } from 'react-hot-toast';
-import { useAuthContext } from './context/AuthContext';
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
   const { authUser } = useAuthContext();
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route
           path="/"
-          element={authUser ? <Home /> : <Navigate to={'/login'} />}
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
         />
         <Route
           path="/login"
