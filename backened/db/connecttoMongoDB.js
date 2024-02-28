03bfca304a25ad26ed46ebfe15ae916af5cad5c7
Shweta <shwetakoffficial@gmail.com>
2024-02-28 13:04:27 +0530

    updated connecttoMongoDb

--- backened/db/connecttoMongoDB.js
+++ backened/db/connecttoMongoDB.js
@@ -1,24 +1,12 @@
-//connect to mongoose
-
-
-import dotenv from "dotenv";
-dotenv.config();
-
 import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI, {
-    
-  
-    });
-    console.log("connected to mongodb");
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.log("error in connecting to mongodb", error.message);
+    console.log("Error connecting to MongoDB", error.message);
   }
 };
-
-// Call the connectToMongoDB function here
-connectToMongoDB();
 
 export default connectToMongoDB;
